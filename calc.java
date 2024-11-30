f472d2ee4fa8a86a57ef85ab4d3a34510ed3ed78
garvit <garvitsharda07@gmail.com>
2024-11-30 12:47:17 +0530

    2nd commit

--- calc.java
+++ calc.java
@@ -3,7 +3,12 @@ public class calc{
         return (a+b);
     }
 
+    public static int sub(int a, int b){
+        return (a-b);
+    }
+
     public static void main(String[] args) {
         System.out.println(add(2,3));
+        System.out.println(sub(2,3));
     }
 }
